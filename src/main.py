8cd4e1f05379b8ce9971a2091700ccf13be1b9b6
Chase <chasebernard8@gmail.com>
2020-08-31 10:14:59 -0500

    Raw skeleton complete.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,21 @@
 import mysql.connector
 
 def main():
-  mydb = mysql.connector.connect(host="localhost", user="chasebernard8",
-  passwd="", database="userlogin")
+  mydb = mysql.connector.connect(host="localhost", user="user",
+  passwd="Password1", database="userlogin")
+
+  mycursor = mydb.cursor()
 
   username = raw_input("Please enter your username")
   passwd = raw_input("Please enter your password")
 
   add_user = ("INSERT INTO userlogin "
- "(username, password) VALUES (" + username + ", " + passwd + ")")
+ "(id, username, password) VALUES (1, " + username + ", " + passwd + ")")
+
+  mycursor.execute(add_user)
 
   mydb.commit()
+  mycursor.close()
   mydb.close()
 
 
