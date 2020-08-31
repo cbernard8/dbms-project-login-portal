867e905db51dd546cd33d785091eec621ba309da
Chase <chasebernard8@gmail.com>
2020-08-31 09:16:12 -0500

    Minor errors corrected.

--- src/main.py
+++ src/main.py
@@ -2,14 +2,13 @@ import mysql.connector
 
 def main():
   mydb = mysql.connector.connect(host="localhost", user="chasebernard8",
-  passwd="", databes="userlogin")
+  passwd="", database="userlogin")
 
   username = raw_input("Please enter your username")
   passwd = raw_input("Please enter your password")
 
   add_user = ("INSERT INTO userlogin "
- "(username, password) "
- "VALUES (" + username + ", " + passwd + ")"
+ "(username, password) VALUES (" + username + ", " + passwd + ")")
 
   mydb.commit()
   mydb.close()
