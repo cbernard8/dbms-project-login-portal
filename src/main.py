cda66b467de3f0f531cc6d6f05eaa1a1633de08f
Chase <chasebernard8@gmail.com>
2020-08-31 09:10:49 -0500

    More raw functionality added.

--- src/main.py
+++ src/main.py
@@ -2,8 +2,18 @@ import mysql.connector
 
 def main():
   mydb = mysql.connector.connect(host="localhost", user="chasebernard8",
-  passwd="")
+  passwd="", databes="userlogin")
 
   username = raw_input("Please enter your username")
   passwd = raw_input("Please enter your password")
 
+  add_user = ("INSERT INTO userlogin "
+ "(username, password) "
+ "VALUES (" + username + ", " + passwd + ")"
+
+  mydb.commit()
+  mydb.close()
+
+
+if __name__ == "__main__":
+  main()
