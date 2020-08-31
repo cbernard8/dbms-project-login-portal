482bfae3441fe77ce0560c4dac0b3534597e2717
Chase <chasebernard8@gmail.com>
2020-08-31 11:12:27 -0500

    Connection to database fixed.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 import mysql.connector
 
 def main():
-  mydb = mysql.connector.connect(host="localhost", user="user",
-  passwd="Password1", database="userlogin")
+  mydb = mysql.connector.connect(host="localhost", user="root",
+  passwd="password", database="userlogin")
 
   mycursor = mydb.cursor()
 
   username = raw_input("Please enter your username")
   passwd = raw_input("Please enter your password")
 
-  add_user = ("INSERT INTO userlogin "
+  add_user = ("INSERT INTO user "
  "(id, username, password) VALUES (1, " + username + ", " + passwd + ")")
 
   mycursor.execute(add_user)
