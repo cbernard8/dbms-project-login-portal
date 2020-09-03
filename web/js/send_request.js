d7d8d414a0ec5cd93ef908a6742c5e976d119691
Chase <chasebernard8@gmail.com>
2020-09-03 15:37:50 -0500

    New files

--- web/js/send_request.js
+++ web/js/send_request.js
@@ -1,7 +1,11 @@
 
-function sendRequest() {
+function sendJSON() {
 
+  let result = document.querySelector('#form1'); 
+  let name = document.querySelector('#name'); 
+  let email = document.querySelector('#email');
 
+  let connection = new WebSocket();
 
 
 
